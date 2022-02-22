9755b9b24082862e4884fa7db52a8b6b22c5be42
Vesselin Velichkov <vesko@lancre>
2022-02-22 09:38:14 +0000

    test plonk with modified sha256

--- libsnark/zk_proof_systems/plonk/tests/test_plonk.cpp
+++ libsnark/zk_proof_systems/plonk/tests/test_plonk.cpp
@@ -2,7 +2,7 @@ template<typename ppT> void test_plonk()
 {
     // Execute all tests for the given curve.
     ppT::init_public_params();
-    printf("[%s:%d] Test OK\n", __FILE__, __LINE__);    
+    printf("[%s:%d] Test OK\n", __FILE__, __LINE__);
 }
 
 TEST(TestPolynomialCommitments, ALT_BN128)
