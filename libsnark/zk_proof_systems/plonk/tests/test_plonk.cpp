09c2c9beeaa7ebe052c293f6b674a1c520a6bd51
Vesselin Velichkov <vesko@lancre>
2022-03-30 11:08:05 +0100

    plonk example

--- libsnark/zk_proof_systems/plonk/tests/test_plonk.cpp
+++ libsnark/zk_proof_systems/plonk/tests/test_plonk.cpp
@@ -17,8 +17,8 @@
 #include <libff/common/utils.hpp>
 #include <gtest/gtest.h>
 #include <libff/algebra/curves/bls12_381/bls12_381_pp.hpp>
-//#include <libsnark/polynomial_commitments/kzg10.hpp>
-//#include <libsnark/polynomial_commitments/tests/polynomial_commitment_test_utils.hpp>
+//#include <libsnark/polynomial_Commitments/kzg10.hpp>
+//#include <libsnark/polynomial_Commitments/tests/polynomial_Commitment_test_utils.hpp>
 
 const size_t MAX_DEGREE = 254;
 
@@ -28,11 +28,11 @@ example circuit
 
 P(x) = x**3 + x + 5 = 3
 
-circuit has 6 gates + 2 dummy gates
+circuit has 6 gates + 2 dummy gates (to make power of 2)
 
-gates
+gates / constraints
 
-      L    R    O
+        L   R    O
 1 mul   x * x  = v1
 2 mul  v1 * x  = v2
 3 add  v2 + x  = v3
@@ -44,19 +44,23 @@ gates
 
 wire polynomials
 
-w_l = [ x, v1, v2,  1,  1, v3,  /,  /] = [a1, a2, a3, a4, a5, a6, a7] = a
-w_r = [ x,  x,  x,  5, 35,  5,  /,  /] = [b1, b2, b3, b4, b5, b6, b7] = b
-w_o = [v1, v2, v3,  5, 35, 35,  /,  /] = [c1, c2, c3, c4, c5, c6, c7] = c
+w_L = [ x, v1, v2,  1,  1, v3,  /,  /] = [a1, a2, a3, a4, a5, a6, a7, a8] = a
+w_R = [ x,  x,  x,  5, 35,  5,  /,  /] = [b1, b2, b3, b4, b5, b6, b7, b8] = b
+w_O = [v1, v2, v3,  5, 35, 35,  /,  /] = [c1, c2, c3, c4, c5, c6, c7, c8] = c
+
+wires = [a1, a2, a3, a4, a5, a6, a7, a8, b1, b2, b3, b4, b5, b6, b7, b8, c1, c2, c3, c4, c5, c6, c7, c8]
+index = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+perm  = [ 9, 17, 18,  5,  4, 19,  7,  8, 10, 11,  1, 14, 21, 20, 15, 16,  2,  3,  6, 12, 22, 13, 23, 24]
 
 witness
 
 x = 3 => v1 = 9, v2 = 27, v3 = 30
 
-w_l = a = [ 3,  9, 27,  1,  1, 30,  0,  0]
-w_r = b = [ 3,  3,  3,  5, 35,  5,  0,  0]
-w_o = c = [ 9, 27, 30,  5, 35, 35,  0,  0]
+w_L = a = [ 3,  9, 27,  1,  1, 30,  0,  0]
+w_R = b = [ 3,  3,  3,  5, 35,  5,  0,  0]
+w_O = c = [ 9, 27, 30,  5, 35, 35,  0,  0]
 
-W = w_l + w_r + w_o
+W = w_L + w_R + w_O
 
 encoding of plonk gates
 
@@ -66,61 +70,128 @@ con5 = [0, 1, 0, 0, 5]
 pi   = [0, 1, 0, 0, 0]
 dum  = [0, 0, 0, 0, 0]
 
-gate polynomials
+gate matrix
 
-(q_L * a) + (q_r * b) + (q_o * c) + (q_m * a * b) + (q_c) = 0
+(q_L * a) + (q_R * b) + (q_O * c) + (q_M * a * b) + (q_C) = 0
 
-                       q_l q_r q_o q_m q_c
+gates                  q_L q_R q_O q_M q_C
 1 mul   x * x  = v1 : [  0,  0, -1,  1,  0]
 2 mul  v1 * x  = v2 : [  0,  0, -1,  1,  0]
 3 add  v2 + x  = v3 : [  1,  1, -1,  0,  0]
 4 con5  1 * 5  = 5  : [  0,  1,  0,  0, -5]
 5 pin   1 * 35 = 35 : [  0,  1,  0,  0,  0]
-6 add  v2 + 5  = 35 : [  1,  1,  0, -1,  0]
+6 add  v2 + 5  = 35 : [  1,  1, -1,  0,  0]
 7 dum   /    /    / : [  0,  0,  0,  0,  0]
 8 dum   /    /    / : [  0,  0,  0,  0,  0]
 
-q_l = [ 0,  0,  1,  0,  0,  1,  0,  0]
-q_r = [ 0,  0,  1,  1,  1,  1,  0,  0]
-q_o = [-1, -1, -1,  0,  0,  0,  0,  0]
-q_m = [ 1,  1,  0,  0,  0, -1,  0,  0]
-q_c = [ 0,  0,  0, -1,  0,  0,  0,  0]
+q_L = [ 0,  0,  1,  0,  0,  1,  0,  0]
+q_R = [ 0,  0,  1,  1,  1,  1,  0,  0]
+q_O = [-1, -1, -1,  0,  0,  0,  0,  0]
+q_M = [ 1,  1,  0,  0,  0, -1,  0,  0]
+q_C = [ 0,  0,  0, -1,  0,  0,  0,  0]
 
+setup
+
+input: 
+- gate matrix
+- permutation vector
+- public input
+
+output: 
+- SRS
+- gate polynomials Q: q_L, q_R, q_O, q_M, q_C
+- public input (PI) polynomial
+- permutation polynomials S_sigma_1, S_sigma_2, S_sigma_3
+- permutation precomputation: id_doman, perm_domain
+
+
+
+permutation polynomials
+
+TBD
 
   auto reference_string = SRS
   proving_key key = 
   program_witness witness = 
 
-  polynomial w_l;
-  polynomial w_r;
-  polynomial w_o;
-  polynomial q_l;
-  polynomial q_r;
-  polynomial q_o;
-  polynomial q_c;
-  polynomial q_m;
+  polynomial w_L;
+  polynomial w_R;
+  polynomial w_O;
+  polynomial q_L;
+  polynomial q_R;
+  polynomial q_O;
+  polynomial q_C;
+  polynomial q_M;
 
 
 */
   
 namespace libsnark
 {
+  template<typename FieldT> using polynomial = std::vector<FieldT>; // kzg10.hpp
 
-template<typename ppT> void test_plonk()
-{
-  //    typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
-  //    default_r1cs_ppzksnark_pp::init_public_params();
-  //  Fp_model;
-
+  //  void compute_qpolynomials();
+  //  void compute_wire_permutation();
+  
+  template<typename ppT> void test_plonk()
+  {
     // Execute all tests for the given curve.
     ppT::init_public_params();
+    
+    using Field = libff::Fr<ppT>;
+
+    // hard-coded gates matrix for the example circuit
+    // P(x) = x**3 + x + 5 = 3
+    std::vector<std::vector<Field>>gates_matrix
+      {
+       // q_L     q_R        q_O         q_M       q_C
+       {Field(0), Field(0), -Field("1"), Field(1),  Field(0)},   // mul
+       {Field(0), Field(0), -Field("1"), Field(1),  Field(0)},   // mul
+       {Field(1), Field(1), -Field("1"), Field(0),  Field(0)},   // add
+       {Field(0), Field(1),  Field(0),   Field(0), -Field("5")}, // con5
+       {Field(0), Field(1),  Field(0),   Field(0),  Field(0)},   // PI
+       {Field(1), Field(1), -Field("1"), Field(0),  Field(0)},   // add
+       {Field(0), Field(0),  Field(0),   Field(0),  Field(0)},   // dummy
+       {Field(0), Field(0),  Field(0),   Field(0),  Field(0)},   // dummy
+      };
+
+    // output from compute_qpoly()
+    polynomial<Field> q_L{   Field(0),    Field(0),    Field(1),    Field(0),  Field(0),    Field(1),  Field(0),  Field(0)};
+    polynomial<Field> q_R{   Field(0),    Field(0),    Field(1),    Field(1),  Field(1),    Field(1),  Field(0),  Field(0)};
+    polynomial<Field> q_O{-Field("1"), -Field("1"), -Field("1"),    Field(0),  Field(0), -Field("1"),  Field(0),  Field(0)};
+    polynomial<Field> q_M{   Field(1),    Field(1),    Field(0),    Field(0),  Field(0),    Field(0),  Field(0),  Field(0)};
+    polynomial<Field> q_C{   Field(0),    Field(0),    Field(0), -Field("5"),  Field(0),    Field(0),  Field(0),  Field(0)};
+
+    // output from compute_permutation()
+    std::vector<int> wire_permutation{9, 17, 18, 5, 4, 19, 7, 8, 10, 11, 1, 14, 21, 20, 15, 16, 2, 3, 6, 12, 22, 13, 23, 24};
+
+    Field public_input = Field(35);
+
+    // index of the row in the gates_matrix
+    int public_input_index = 4;
+
+    // output from compute_omega_base()
+    // Example (2**32)-th primitive root of unity in the base field Fq
+    // of bls12-381 i.e. such that omega_base**(2**32) = 1. The
+    // bls12-381 prime q is such that any power of 2 divides (q-1). In
+    // particular 2**32|(q-1) and so the 2**32-th root of unity
+    // exists.
+    Field omega_base = Field("5398635374615924329006194896463915232623626471712994625313653909674492148212");
+
+    // Generate the n-th root of unity omega in Fq (n=8 in the
+    // example) using omega_base. omega is a generator of the
+    // multiplicative subgroup H.
+    Field temp = libff::power(omega_base, libff::bigint<1>(1));
+    assert(temp == omega_base);
+    
+    
     //    assert(0);
     printf("[%s:%d] Test OK\n", __FILE__, __LINE__);
-}
+  }
 
-TEST(TestPlonk, BLS12_381)
-{
+  TEST(TestPlonk, BLS12_381)
+  {
     test_plonk<libff::bls12_381_pp>();
-}
+  }
 
 } // namespace libsnark
