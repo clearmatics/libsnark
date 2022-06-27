591ec92b67c7615c06fd884d29e518e6c3b8b44c
Vesselin Velichkov <vesselin@lancre>
2022-06-27 11:51:18 +0100

    plonk: implemented srs class

---
+++ libsnark/zk_proof_systems/plonk/srs.hpp
@@ -0,0 +1,195 @@
+/** @file
+ *****************************************************************************
+ * @author     This file is part of libff, developed by Clearmatics Ltd
+ *             (originally developed by SCIPR Lab) and contributors
+ *             (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#ifndef LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_HPP_
+#define LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_HPP_
+
+/// Declaration of SRS interfaces for ppzkSNARK proof system Plonk. This
+/// includes:
+///
+/// - class usrs
+/// - class srs
+/// - class for proving key (TODO: not implemented)
+/// - class for verification key (TODO: not implemented)
+/// - class for key pair (proving key & verification key) (TODO: not
+///   implemented)
+///
+/// Reference:
+/// - \[GWC19]:
+///   Title: "Plonk: Permutations over lagrange-bases for oecumenical
+///   noninteractive arguments of knowledge", Ariel Gabizon, Zachary
+///   J. Williamson, and Oana Ciobotaru, Cryptology ePrint Archive,
+///   Report 2019/953, 2019, <https://eprint.iacr.org/2019/953>
+
+namespace libsnark
+{
+
+///
+/// A note on the distinction between an srs and a universal srs.
+///
+/// A universal srs (usrs) is composed *only* of monomials i.e. encoded
+/// powers of the secret value in the group generator. Therefore a usrs
+/// is independent of any particular circuit.
+///
+/// The (plain) srs is a specialization of the usrs for one particular
+/// circuit and is derived from the usrs e.g. as
+///
+/// usrs = <encoded powers of secret>
+/// srs = (proving_key, verificataion_key) = derive(usrs, circuit_description)
+///
+
+/// Universal srs (usrs). Contains secret encoded monomials with
+/// maximum degree MAX_DEGREE and is so independent of any particular
+/// circuit.
+template<typename ppT> class usrs
+{
+public:
+    /// Array of powers of secret \secret, encoded in G1:
+    /// [1]_1, [\secret]_1, [\secret^2]_1, ..., [\secret^{n+2}]_1
+    std::vector<libff::G1<ppT>> secret_powers_g1;
+
+    /// Array of powers of secret \secret, encoded in G2:
+    /// [1]_2, [\secret]_2
+    std::vector<libff::G2<ppT>> secret_powers_g2;
+
+    usrs(
+        std::vector<libff::G1<ppT>> &&secret_powers_g1,
+        std::vector<libff::G2<ppT>> &&secret_powers_g2)
+        : secret_powers_g1(secret_powers_g1), secret_powers_g2(secret_powers_g2)
+    {
+    }
+};
+
+template<typename ppT>
+usrs<ppT> plonk_usrs_derive_from_secret(const libff::Fr<ppT> &secret);
+
+/// Plain (i.e. non-universal srs). Contains secret encoded monomials
+/// with maximum degree equal to the number of gates of the analyzed
+/// circuit + 2, plus circuit description. Dependent on the circuit.
+template<typename ppT> class srs
+{
+public:
+    using Field = libff::Fr<ppT>;
+    /// number of gates in the analyzed circuit, denoted by "n" in
+    /// [GWC19]
+    size_t num_gates;
+    /// number of selector polynomials (q-polynomials) (= 5 in the
+    /// vanilla Plonk proposal [GWC19])
+    size_t num_qpolys;
+    /// Lagrange basis
+    std::vector<polynomial<Field>> L_basis;
+    /// Public input polynomial
+    polynomial<Field> PI_poly;
+    /// Circuit selector polynomials (Q-polynomials)
+    std::vector<polynomial<Field>> Q_polys;
+    /// Permutation polynomials S_sigma_1, S_sigma_2, S_sigma_2 (see
+    /// [GWC19], Sect. 8.1)
+    std::vector<polynomial<Field>> S_polys;
+    /// omega[0] are the n roots of unity, omega[1] are omega[0]*k1,
+    /// omega[2] are omega[0]*k2
+    std::vector<std::vector<Field>> omega_roots;
+    /// H_gen contains the generators of H, k1 H and k2 H in one place
+    /// ie. omega, omega_k1 and omega_k2
+    std::vector<Field> H_gen;
+    /// H_gen permuted according to the wire permutation
+    std::vector<Field> H_gen_permute;
+    /// constants for H, k1 H, k2 H
+    libff::Fr<ppT> k1;
+    libff::Fr<ppT> k2;
+
+    /// Array of powers of secret \alpha, encoded in G1: [1]_1,
+    /// [\alpha]_1, [\alpha^2]_1, ..., [\alpha^{n+2}]_1
+    std::vector<libff::G1<ppT>> secret_powers_g1;
+
+    /// Array of powers of secret \alpha, encoded in G2: [1]_2,
+    /// [\alpha]_2
+    std::vector<libff::G2<ppT>> secret_powers_g2;
+
+    srs(const size_t &num_gates,
+        const size_t &num_qpolys,
+        const std::vector<polynomial<Field>> &L_basis,
+        const polynomial<Field> &PI_poly,
+        const std::vector<polynomial<Field>> &Q_polys,
+        const std::vector<polynomial<Field>> &S_polys,
+        const std::vector<std::vector<Field>> &omega_roots,
+        const std::vector<Field> &H_gen,
+        const std::vector<Field> &H_gen_permute,
+        const libff::Fr<ppT> &k1,
+        const libff::Fr<ppT> &k2,
+        std::vector<libff::G1<ppT>> &&secret_powers_g1,
+        std::vector<libff::G2<ppT>> &&secret_powers_g2)
+        : num_gates(num_gates)
+        , num_qpolys(num_qpolys)
+        , L_basis(L_basis)
+        , PI_poly(PI_poly)
+        , Q_polys(Q_polys)
+        , S_polys(S_polys)
+        , omega_roots(omega_roots)
+        , H_gen(H_gen)
+        , H_gen_permute(H_gen_permute)
+        , k1(k1)
+        , k2(k2)
+        , secret_powers_g1(secret_powers_g1)
+        , secret_powers_g2(secret_powers_g2)
+    {
+    }
+};
+
+template<typename ppT>
+srs<ppT> plonk_srs_derive_from_usrs(
+    const usrs<ppT> &usrs, const circuit_t<ppT> &circuit);
+
+/// A proving key for Plonk
+template<typename ppT> class plonk_proving_key
+{
+public:
+    /// Array of powers of secret \alpha, encoded in G1: [1]_1,
+    /// [\alpha]_1, [\alpha^2]_1, ..., [\alpha^{n+2}]_1
+    std::vector<libff::G1<ppT>> secret_powers_g1;
+
+    plonk_proving_key(){};
+    plonk_proving_key(std::vector<libff::G1<ppT>> &&secret_powers_g1)
+        : secret_powers_g1(std::move(secret_powers_g1)){};
+};
+
+/// A verification key for Plonk
+template<typename ppT> class plonk_verification_key
+{
+public:
+    /// Array of powers of secret \alpha, encoded in G2: [1]_2,
+    /// [\alpha]_2
+    std::vector<libff::G2<ppT>> secret_powers_g2;
+
+    plonk_verification_key();
+    plonk_verification_key(std::vector<libff::G2<ppT>> &&secret_powers_g2)
+        : secret_powers_g2(std::move(secret_powers_g2)){};
+};
+
+/// A key pair for Plonk, which consists of a proving key and a
+/// verification key.
+template<typename ppT> class plonk_keypair
+{
+public:
+    plonk_proving_key<ppT> pk;
+    plonk_verification_key<ppT> vk;
+
+    plonk_keypair() = default;
+    plonk_keypair(const plonk_keypair<ppT> &other) = default;
+    plonk_keypair(plonk_proving_key<ppT> &&pk, plonk_verification_key<ppT> &&vk)
+        : pk(std::move(pk)), vk(std::move(vk))
+    {
+    }
+
+    plonk_keypair(plonk_keypair<ppT> &&other) = default;
+};
+
+} // namespace libsnark
+
+#include "libsnark/zk_proof_systems/plonk/srs.tcc"
+
+#endif // LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_HPP_
---
+++ libsnark/zk_proof_systems/plonk/srs.tcc
@@ -0,0 +1,104 @@
+/** @file
+ *****************************************************************************
+ * @author     This file is part of libff, developed by Clearmatics Ltd
+ *             (originally developed by SCIPR Lab) and contributors
+ *             (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#ifndef LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_TCC_
+#define LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_TCC_
+
+/// Implementation of SRS interfaces for a ppzkSNARK for Plonk. See
+/// srs.hpp .
+
+namespace libsnark
+{
+
+/// Compute a universal srs (usrs). It is composed *only* of encoded
+/// powers of the secret value in the group generator. Therefore a usrs
+/// is independent of any particular circuit.
+///
+/// \note only for debug
+template<typename ppT>
+usrs<ppT> plonk_usrs_derive_from_secret(const libff::Fr<ppT> &secret)
+{
+    // compute powers of secret times G1: 1*G1, secret^1*G1, secret^2*G1, ...
+    const libff::bigint<libff::Fr<ppT>::num_limbs> secret_bigint =
+        secret.as_bigint();
+    const size_t window_size = std::max(
+        libff::wnaf_opt_window_size<libff::G1<ppT>>(secret_bigint.num_bits()),
+        1ul);
+    const std::vector<long> naf =
+        libff::find_wnaf<libff::Fr<ppT>::num_limbs>(window_size, secret_bigint);
+
+    std::vector<libff::G1<ppT>> secret_powers_g1;
+    secret_powers_g1.reserve(MAX_DEGREE);
+    libff::G1<ppT> secret_i_g1 = libff::G1<ppT>::one();
+    secret_powers_g1.push_back(secret_i_g1);
+    for (size_t i = 1; i < MAX_DEGREE; ++i) {
+        // secret^i * G1
+        secret_i_g1 = libff::fixed_window_wnaf_exp<libff::G1<ppT>>(
+            window_size, secret_i_g1, naf);
+        secret_powers_g1.push_back(secret_i_g1);
+    }
+
+    // compute powers of secret times G2: 1*G2, secret^1*G2
+    // Note: in Plonk we *always* have 2 encoded elemnts in G2
+    std::vector<libff::G2<ppT>> secret_powers_g2;
+    secret_powers_g2.reserve(2);
+    // secret^0 * G2 = G2
+    libff::G2<ppT> secret_0_g2 = libff::G2<ppT>::one();
+    secret_powers_g2.push_back(secret_0_g2);
+    // secret^1 * G2
+    libff::G2<ppT> secret_1_g2 = secret * libff::G2<ppT>::one();
+    secret_powers_g2.push_back(secret_1_g2);
+
+    return usrs<ppT>(std::move(secret_powers_g1), std::move(secret_powers_g2));
+}
+
+/// Derive the (plain) SRS from the circuit description and the
+/// USRS. The (plain) SRS is a specialization of the USRS for one
+/// particular circuit i.e.
+///
+/// usrs = <encoded powers of secret>
+/// srs = (proving_key, verificataion_key) = derive(usrs, circuit_description)
+template<typename ppT>
+srs<ppT> plonk_srs_derive_from_usrs(
+    const usrs<ppT> &usrs, const circuit_t<ppT> &circuit)
+{
+    assert(circuit.num_gates <= MAX_DEGREE);
+    // secret^i * G1
+    std::vector<libff::G1<ppT>> secret_powers_g1;
+    secret_powers_g1.reserve(circuit.num_gates + 3);
+    for (size_t i = 0; i < (circuit.num_gates + 3); ++i) {
+        secret_powers_g1.push_back(usrs.secret_powers_g1[i]);
+    }
+    // secret^i * G2
+    std::vector<libff::G2<ppT>> secret_powers_g2;
+    secret_powers_g2.reserve(2);
+    for (size_t i = 0; i < 2; ++i) {
+        secret_powers_g2.push_back(usrs.secret_powers_g2[i]);
+    }
+
+    srs<ppT> srs(
+        circuit.num_gates,
+        circuit.num_qpolys,
+        circuit.L_basis,
+        circuit.PI_poly,
+        circuit.Q_polys,
+        circuit.S_polys,
+        circuit.omega_roots,
+        circuit.H_gen,
+        circuit.H_gen_permute,
+        circuit.k1,
+        circuit.k2,
+        std::move(secret_powers_g1),
+        std::move(secret_powers_g2));
+
+    return srs;
+}
+
+} // namespace libsnark
+
+#endif // LIBSNARK_ZK_PROOF_SYSTEMS_PLONK_SRS_TCC_
